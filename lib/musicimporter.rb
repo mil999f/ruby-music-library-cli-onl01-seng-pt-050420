c039728afd9e0a08761f778e22e27392aa6a0226
Milly Flores <miflo0h@gmail.com>
2020-06-24 04:54:41 +0000

    Automatically backed up by Learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -9,7 +9,7 @@ class MusicImporter
     def files  # loads all the MP3 files in the path directory, normalizes the filename to just the MP3 filename with no path
         @files ||= Dir.glob("#{@path}/*.mp3").collect{ |file| file.gsub("#{@path}/", "") }
     end 
-https://learn.co/tracks/online-software-engineering-structured/object-oriented-ruby/section-17-final-projects/music-library-cli#
+
     def import #   imports the files into the library by invoking Song.create_from_filename
         files.each {|file| Song.create_from_filename(file)}
       end
